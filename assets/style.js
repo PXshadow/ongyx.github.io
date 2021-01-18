4842ba6fda469fa0a11592cf9d04d3e69b664bec
Ong Yong Xin <ongyongxin2020+github@gmail.com>
2021-01-18 20:22:40 +0800

    add dark mode toggle

---
+++ assets/style.js
@@ -0,0 +1,41 @@
+// Toggles dark mode and stuff.
+// css-tricks.com/a-complete-guide-to-dark-mode-on-the-web
+
+function getTheme() {
+  return localStorage.getItem("theme");
+}
+
+function setTheme(theme) {
+  if theme == "light" {
+    var counter = 0;
+  } else {
+    var counter = 1;
+  }
+
+  while (counter < 2) {
+    document.documentElement.classList.toggle("dark-mode");
+  }
+  
+  localStorage.setItem("theme", theme);
+}
+
+const preferredTheme = getTheme()
+
+if not (preferredTheme) and (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+  preferredTheme = "dark";
+} else {
+  preferredTheme = "light";
+}
+
+setTheme(preferredTheme);
+
+function toggleTheme() {
+  currentTheme = getTheme()
+  if currentTheme == "dark" {
+    var changedTheme = "light"
+  } else {
+    var changedTheme = "dark"
+  }
+
+  setTheme(changedTheme)
+}
