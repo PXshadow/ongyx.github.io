3b5c6078bf58780799d36a38217fb606781d601d
Ong Yong Xin <ongyongxin2020+github@gmail.com>
2021-01-18 20:50:40 +0800

    fix javascript

--- assets/style.js
+++ assets/style.js
@@ -1,25 +1,24 @@
 // Toggles dark mode and stuff.
 // css-tricks.com/a-complete-guide-to-dark-mode-on-the-web
 
+
+function toggleTheme() {
+  document.documentElement.classList.toggle("dark-mode");
+}
+
 function getTheme() {
   return localStorage.getItem("theme");
 }
 
 function setTheme(theme) {
-  if (theme == "light") {
-    var counter = 0;
-  } else {
-    var counter = 1;
+  if (theme == getTheme()) {
+    return;
   }
-
-  while (counter < 2) {
-    document.documentElement.classList.toggle("dark-mode");
-  }
-  
+  toggleTheme();
   localStorage.setItem("theme", theme);
 }
 
-var preferredTheme = getTheme()
+var preferredTheme = getTheme();
 
 if (!preferredTheme && window.matchMedia("(prefers-color-scheme: dark)").matches) {
   preferredTheme = "dark";
@@ -28,14 +27,3 @@ if (!preferredTheme && window.matchMedia("(prefers-color-scheme: dark)").matches
 }
 
 setTheme(preferredTheme);
-
-function toggleTheme() {
-  currentTheme = getTheme()
-  if (currentTheme == "dark") {
-    var changedTheme = "light"
-  } else {
-    var changedTheme = "dark"
-  }
-
-  setTheme(changedTheme)
-}
